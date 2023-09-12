aa6bb617863cca882a09d69cbc37ac1e07fcf088
Rahat73 <rahat.ashik.18@gmail.com>
2023-09-13 02:53:35 +0600

    changed services/:id with serviceBooking/:id api

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/serviceBooking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const options = {
         projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
