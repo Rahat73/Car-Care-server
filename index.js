6edb6289cae6741572d8314eabfe1e74464347bd
Rahat73 <rahat.ashik.18@gmail.com>
2023-09-12 14:41:15 +0600

    bookings apis

--- index.js
+++ index.js
@@ -41,9 +41,40 @@ async function run() {
       res.send(result);
     });
 
+    //bookings
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { customerEmail: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const update = req.body;
+      console.log(update);
+      const updateDoc = {
+        $set: {
+          status: update.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
