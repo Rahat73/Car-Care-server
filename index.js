05025a9beb86b40301f3ec1a05b304979e1e7ef3
Rahat73 <rahat.ashik.18@gmail.com>
2023-09-14 21:28:34 +0600

    finally vercel working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-var jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
